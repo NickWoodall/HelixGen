tf backprop ready to test, fix occasional nans in loss

Buttress.py
@@ -103,7 +103,9 @@ def buttLoss(recon,mask,refMap,input_z=None,rate=0.05,batch_size=32,cycles=100):
             masked_loss = maskLoss(v,g_o,m2)
 
         g_grads = g_tape.gradient(masked_loss, input_z_var)
-
+        
+        ##### save for when I have to time to check changes
+        #this needs to be fixed, ?lower rate? and and move object creation out of loop
         optimizer = tf.keras.optimizers.SGD(learning_rate=rate)
         #optimizer = tf.keras.optimizers.Adadelta(learning_rate=1.0, rho=0.95)
         #optimizer = tf.keras.optimizers.Adam()

GenerateEndpoints.py
@@ -41,7 +41,7 @@ stub = tt[7:10].reshape(15,4)
 
 class BatchRecon():
     
-    def __init__(self, name,size=8, h4_feats=28):
+    def __init__(self, name, size=8, h4_feats=28, z_size = 12):
         """Load generator to make endpoints. Make sure MinMaxScaler is in the same directory with _mm added to end."""
         self.name = name
         self.g=tf.keras.models.load_model(f'{name}.h5')
@@ -52,6 +52,7 @@ class BatchRecon():
         self.size = size
         self.feats = h4_feats
         self.iu1 = np.triu_indices(self.size, 1)
+        self.z_size = 12
 
     
     def generate(self,z,input_z=None,batch_size=24):